dbab644b3a6eb68260ad3c1ad4b02ef9f3b9cc02
gabriel <g.martin@htlkrems.at>
2026-01-13 12:18:30 +0100

    Init

---
+++ Source/GSS.CLI/Program.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+Version version = Assembly.GetExecutingAssembly().GetName().Version!;
+
+Console.WriteLine($"GSS CLI - Version {version.Major}.{version.Minor}.{version.Build}");
